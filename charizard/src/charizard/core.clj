a9dc252958df8be652cf6e0d7165211f9a7b5bf2
rodrigofudissaku@gmail.com <rodrigofudissaku@gmail.com>
2020-07-05 12:32:50 -0300

    Create a sample insert

--- charizard/src/charizard/core.clj
+++ charizard/src/charizard/core.clj
@@ -1,8 +1,16 @@
 (ns charizard.core
- 
+  (:require [datomic.client.api :as d])
+  (:require [charizard.repository.schema])
+  (:require [charizard.repository.mapper])
+  (:require [charizard.repository.clojure])
   (:gen-class))
 
-
+(defn start []
+ (do charizard.repository.clojure/init 
+     (-> (charizard.domain.user/->UserEntity (bigint 3) "Rodrigo" "Fudissaku" "mari@gmail.com")
+         charizard.repository.mapper/toEntity
+         charizard.repository.clojure/transact)) 
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
